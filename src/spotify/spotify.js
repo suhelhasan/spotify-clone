98306f65fea7119998eab9a09a477969263a91c3
suhel hasan <suhelhasan202@gmail.com>
2020-08-29 01:49:44 +0530

    cfdfdgg
    Signed-off-by: suhel hasan <suhelhasan202@gmail.com>

--- src/spotify/spotify.js
+++ src/spotify/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // const redirectUri = "https://spotifyplayer.netlify.app/";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotifyplayer.netlify.app/";
 const clientId = "7decccf815164fe8aaeb6046b236c643";
 
 const scopes = [
